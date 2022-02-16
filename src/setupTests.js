3222610a4d6ae1d37625526dee032b0797bca559
cataopriscila <cataopriscila@gmail.com>
2022-02-16 15:35:46 -0300

    test: add automation tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom';
+
+import App from './App';
+import renderer from 'react-test-renderer';
+
+describe('<App />', () => {
+  it('has 2 children', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(2);
+  });
+});
